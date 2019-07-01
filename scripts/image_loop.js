781fa55fa74156520a929d06757a79ebf646fe31
Andrew S Allen <andrew.allen91@gmail.com>
2019-07-01 13:41:13 -0400

    added readme, changed image loop

--- scripts/image_loop.js
+++ scripts/image_loop.js
@@ -10,5 +10,5 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 7500); // 7.5 seconds
+    setTimeout(showSlides, 6000); // 6 seconds
 }
