dfd50378489478a9772c9a5d82247d9ff0b97087
Andrew S Allen <andrew.allen91@gmail.com>
2019-07-16 18:25:40 -0400

    final commit

--- scripts/image_loop.js
+++ scripts/image_loop.js
@@ -1,14 +1,17 @@
 var slideIndex = 0;
 showSlides();
-
+// This Function targets multiple variables
 function showSlides() {
     var i;
+    // Target the specific class "slideshow"
     var slides = document.getElementsByClassName("slideshow");
+    // Apply a for loop to cycle through selected images
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display="none";
     }
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 6000); // 6 seconds
+    // Wait 6 seconds to cycle to the next image
+    setTimeout(showSlides, 6000);
 };
--- scripts/scrolling.js
+++ scripts/scrolling.js
@@ -1,7 +1,10 @@
+//Target all anchor tags
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    //On click
     anchor.addEventListener('click', function (e) {
+        // Prevent default "click" behavior
         e.preventDefault();
-
+        // Apply a smoother scrolling effect 
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
